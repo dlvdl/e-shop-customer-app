4472cb8d7e9ad8b5431d1ed97fc934ed31d54dc5
Denys <denisligach98@gmail.com>
2023-11-15 14:52:58 +0200

    added index file in images

---
+++ src/assets/index.tsx
@@ -0,0 +1,31 @@
+import Cart from "./cart.svg"
+import Search from "./search.svg"
+import Profile from "./profile.svg"
+import LogoHeader from "./3legant..svg"
+import ImagePlaceholder from "./placeholder.png"
+import FilterIcon from "./filter.svg"
+import Grid_1 from "./grid-1.svg"
+import Grid_2 from "./grid-2.svg"
+import Grid_3 from "./grid-3.svg"
+import Grid_4 from "./grid-4.svg"
+import Star_f from "./star_active.svg"
+import Star_h from "./Star_disebled.svg"
+import P from "./069a0857fe7da4976ba73bf5db64a055.jpeg"
+import Heart from "./heart.svg"
+
+export {
+  FilterIcon,
+  Cart,
+  Search,
+  Profile,
+  LogoHeader,
+  ImagePlaceholder,
+  Grid_1,
+  Grid_2,
+  Grid_3,
+  Grid_4,
+  Star_f,
+  Star_h,
+  P,
+  Heart,
+}
