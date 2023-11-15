073822ab6863753c321f5c5a3fb45e2eaae6db11
Denys <denisligach98@gmail.com>
2023-11-15 14:57:22 +0200

    index file for componets folder

---
+++ src/components/index.tsx
@@ -0,0 +1,8 @@
+import Header from "./Header"
+import BackgroundBox from "./BackgroundBox"
+import Shop from "./Shop"
+
+import CustomListBox from "./CustomListBox"
+import CustomCheckBox from "./CustomCheckBox"
+
+export { Header, BackgroundBox, Shop, CustomListBox, CustomCheckBox }
